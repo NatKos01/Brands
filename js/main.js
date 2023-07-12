2571c76f9978c0e9e5e1d840d0a442e08d0a0918
NatKos01 <138513396+NatKos01@users.noreply.github.com>
2023-07-12 06:07:19 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,4 +10,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
     
 })
+const footerYear = document.querySelector('.footer__year');
+
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
 
